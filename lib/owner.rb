1c0c2bcea7a9a69c401f3ac6cc6234bf1a4d4209
Gabriela Suriano <surianogabriela@gmail.com>
2018-09-12 10:40:37 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -73,4 +73,13 @@ class Owner
     end
   end
   
+  def sell_pets 
+    @pets.collect do |species, instances|
+      instances.each do |pet|
+        pet.mood = "nervous"
+      end
+      instances.clear 
+    end 
+  end 
+  
 end
